b422268cda5f1dbae650c816f40f5160908ecd0d
batovpasha <batovpasha@gmail.com>
2020-12-30 01:55:42 +0200

    change default REQUESTS_INTERVAL value to 60 seconds

--- config/hypes.js
+++ config/hypes.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     BASE_API_URL      : process.env.IEX_STOCK_BASE_API_URL || 'https://cloud.iexapis.com/stable/stock',
     HYPES_PATH        : '/news',
     // eslint-disable-next-line no-magic-numbers
-    REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 1000
+    REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 60000
 });
