a3b7e29e679d909733b06d2ce2a9b3b2b3eb730b
batovpasha <batovpasha@gmail.com>
2020-12-29 23:27:08 +0200

    decompose main IEX api configs from stockPrice, add hypes API configs

--- config/hypes.js
+++ config/hypes.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    STOCK_PRICE_PATH  : '/quote/latestPrice',
+    HYPES_PATH        : '/news',
     // eslint-disable-next-line no-magic-numbers
     REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 1000
 });
---
+++ config/iex.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    IEX_TOKEN    : process.env.IEX_TOKEN,
+    BASE_API_URL : process.env.IEX_STOCK_BASE_API_URL || 'https://sandbox.iexapis.com/stable/stock/'
+});
