72a1e7a2987a5b6034b52aae8f7e0f7e6f4d787d
batovpasha <batovpasha@gmail.com>
2020-12-30 16:18:50 +0200

    separate each token for hypes and stockPrice configs, hypes stream improvements

--- config/hypes.js
+++ config/hypes.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
     BASE_API_URL      : process.env.IEX_STOCK_BASE_API_URL || 'https://cloud.iexapis.com/stable/stock',
+    TOKEN             : process.env.IEX_TOKEN,
     HYPES_PATH        : '/news',
     // eslint-disable-next-line no-magic-numbers
     REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 60000
--- config/stockPrice.js
+++ config/stockPrice.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
-    BASE_API_URL      : process.env.IEX_STOCK_BASE_API_URL || 'https://cloud.iexapis.com/stable/stock',
+    BASE_API_URL      : process.env.IEX_STOCK_BASE_API_URL || 'https://sandbox.iexapis.com/stable/stock',
+    TOKEN             : process.env.IEX_SANDBOX_TOKEN,
     STOCK_PRICE_PATH  : '/quote/latestPrice',
     // eslint-disable-next-line no-magic-numbers
-    REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 1000
+    REQUESTS_INTERVAL : process.env.STOCK_PRICE_REQUESTS_INTERVAL || 15000
 });
